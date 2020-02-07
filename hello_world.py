1fd1de6b9656fc4f998628ca1b2a4fc9c4690937
dirk <dirk.degel.psa@gmail.com>
2020-02-07 18:00:20 +1100

    test

---
+++ hello_world.py
@@ -0,0 +1,5 @@
+# /Library/Frameworks/Python.framework/Versions/3.8/bin/python3.8
+
+print("Hello World!")
+import sys
+print(sys.version)
---
+++ python_information.md
@@ -0,0 +1,5 @@
+# Version
+
+Python 3.8.1 https://www.python.org/
+
+# IDE
